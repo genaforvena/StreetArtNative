767fba170bed44aa4b564f34a723d8876bea5768
Ilya Mozerov <ilyailiilia@gmail.com>
2016-10-13 23:15:12 +0300

    Handling item click

--- app/ArtList.js
+++ app/ArtList.js
@@ -1,52 +1,65 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet,
-  ListView,
-  TouchableHighlight,
-  Image,
-  Text,
-  Navigator,
-  View
+    StyleSheet,
+    ListView,
+    TouchableHighlight,
+    Image,
+    Text,
+    Navigator,
+    View
 } from 'react-native';
 
 import { ArtObjectInList } from './ArtObjectInList'
 import fetchArtObjects from './fetch'
 
 export default class ArtList extends Component {
-  constructor(props) {
-    super(props);
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-        dataSource: ds.cloneWithRows([])
-    };
+    constructor(props) {
+        super(props);
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.state = {
+            dataSource: ds.cloneWithRows([])
+        };
 
-    fetchArtObjects((json) => {
-        this.setState({ dataSource : this.state.dataSource.cloneWithRows(json)});
-    });
-  }
+        fetchArtObjects((json) => {
+            this.setState({ dataSource : this.state.dataSource.cloneWithRows(json)});
+        });
+    }
 
-  render() {
-    return (
-          <ListView
-            contentContainerStyle={styles.list}
-            dataSource={this.state.dataSource}
-            renderRow={this.renderRowData}
-          />
-    );
-  }
+    render() {
+        return (
+            <ListView
+                contentContainerStyle={styles.list}
+                dataSource={this.state.dataSource}
+                renderRow={this.renderRowData}
+                />
+        );
+    }
 
-  renderRowData(rowData) {
-      let url = rowData.photos[0].image
-      return (
-          <ArtObjectInList image={url}/>
-      )
-  }
+    renderRowData(rowData) {
+        let url = rowData.photos[0].image
+        return (
+            <TouchableHighlight
+                style={styles.itemContainer}
+                onPress={() => {
+                    console.log(rowData);
+                }}>
+                <View style={{ flex: 1 }}>
+                    <ArtObjectInList image={url}/>
+                </View>
+            </TouchableHighlight>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  list: {
-    paddingTop: 22,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  }
+    itemContainer: {
+        padding: 1,
+        height: 122,
+        width: 122
+    },
+    list: {
+        paddingTop: 22,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    }
 });
--- app/ArtObjectInList.js
+++ app/ArtObjectInList.js
@@ -2,30 +2,23 @@
 
 import React, { Component } from 'react';
 import {
-  StyleSheet,
-  View,
-  Image,
-  Text
+    StyleSheet,
+    View,
+    Image,
+    Text
 } from 'react-native';
 
 export class ArtObjectInList extends Component {
     render() {
         return (
-            <View style = {styles.container}>
-                <Image
-                    style = {styles.image}
-                    source = {{uri: this.props.image}}/>
-            </View>
+            <Image
+                style = {styles.image}
+                source = {{uri: this.props.image}}/>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 1,
-        height: 122,
-        width: 122
-    },
     image: {
         flex: 1
     }
