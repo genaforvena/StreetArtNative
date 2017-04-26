3cc170ff1a34340f1a9da06854bbf451247644a5
Ilya Mozerov <ilyailiilia@gmail.com>
2017-04-27 00:27:30 +0300

    Fixed image prefetch error handling

--- app/fetch.js
+++ app/fetch.js
@@ -7,14 +7,10 @@ export default async function fetchArtObjects(callback) {
         let response = await fetch("https://imozerov.pythonanywhere.com/artworks/api/v1.0/artworks");
         let json = await response.json();
         let artworks = json.artworks;
-        try {
-            await artworks.forEach(object => {
-                let image = object.image;
-                Image.prefetch(image);
-            });
-        } catch (err) {
-            console.log(err);
-        }
+        await artworks.forEach(object => {
+            let image = object.image;
+            Image.prefetch(image).catch(error => console.log(error));
+        });
         callback(artworks);
     } catch (error) {
         console.log(error);
